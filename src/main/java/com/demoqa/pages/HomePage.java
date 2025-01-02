24237a6c446aaee23efc9c88ff996e7e60f78c87
Lubna Reshi <lubna.riyaz122@gmail.com>
2025-01-02 19:36:02 +0530

    Commit 7

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -1,7 +1,16 @@
 package com.demoqa.pages;
 
+import com.demoqa.pages.forms.FormsPage;
 import com.saucedemo.pages.BasePage;
+import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
+    private By formsCard = By.xpath("//div[@id='app']//h5[text()='Forms']");
+
+    public FormsPage goToForms() {
+        click(formsCard);
+        return new FormsPage();
+
+    }
 }
---
+++ src/test/java/part3_4/com/demoqa/tests/part3/javascript/JavaScriptTest.java
@@ -0,0 +1,7 @@
+package part3_4.com.demoqa.tests.part3.javascript;
+
+import part3_4.com.demoqa.base.BaseTest;
+
+public class JavaScriptTest extends BaseTest {
+
+}
