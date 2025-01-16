eea538cd77d3f7f0584eba7946237838c0d6c9e9
Lubna Reshi <lubna.riyaz122@gmail.com>
2025-01-16 19:26:53 +0530

    Commit 10: v) Links Done

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -8,12 +8,19 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class ElementsPage extends HomePage {
 
     private By webTablesMenuItem = By.xpath("//li[@id='item-3']/span[text()='Web Tables']");
+    private By linksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
 
 
     public WebTablesPage clickWebTables() {
         scrollToElementJS(webTablesMenuItem);
         click(webTablesMenuItem);
         return new WebTablesPage();
+    }
+
+
+    public LinksPage clickLinks() {
+        click(linksMenuItem);
+        return new LinksPage();
     }
 
 }
